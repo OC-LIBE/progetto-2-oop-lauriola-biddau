584025207adc54b1d4bf81908771155170f6fdc1
Emanuele Biddau <emabiddau@gmail.com>
2025-01-09 23:02:26 +0100

    Puntata

--- app.py
+++ app.py
@@ -8,31 +8,47 @@ st.set_page_config(
 card_width=105
 
 
-number_of_decks = st.number_input("Number of decks", min_value=1, max_value=1, value=1)
+# number_of_decks = st.number_input("Number of decks", min_value=1, max_value=1, value=1)
 
-deck = Deck(number_of_decks)
+deck = Deck(1)
 
 
-st.markdown(f"## Deck created with {number_of_decks} deck/s")
+# st.markdown(f"## Deck created with {number_of_decks} deck/s")
 
-st.image([card.image for card in deck.cards], width=card_width)
+# st.image([card.image for card in deck.cards], width=card_width)
 
-st.markdown("## Shuffling deck")
-shuffle_button = st.button("Shuffle")
-if shuffle_button:
+if st.button("Shuffle"):
     deck.shuffle()
-st.image([card.image for card in deck.cards], width=card_width)
+# st.image([card.image for card in deck.cards], width=card_width)
 
-player_money: float = 2000
+@st.dialog("Place a bet")
+def bet():
+    with st.form("Bet", clear_on_submit=True, enter_to_submit=False, border=False):
 
-bet = st.number_input("Bet", min_value=1, max_value=500, value=1)
+        bet = st.number_input("", min_value=1, max_value=500, value=1, key="bet")
+
+        if st.form_submit_button("Invia"):
+            st.rerun()
+
+
+if st.button("Place a bet"):
+    bet()
+
+
+try:
+    st.text(f"The player has bet: {st.session_state['bet']}")
+except:
+    pass
+
 
 playerCards: list[Card] = []
 botCards: list[Card] = []
 
 for i in range(2):
-    playerCards = deck.draw()
-    botCards = deck.draw()
+    playerCards.append(deck.draw())
+    botCards.append(deck.draw())
 
 st.image([card.image for card in playerCards], width=50)
 st.image([card.image for card in botCards], width=50)
+
+st.write(st.session_state)
--- rapporto.md
+++ rapporto.md
@@ -1,9 +1,22 @@
-08.01.2025:
-    Blackjack
+# Rapporto
 
-    importate le immagini delle carte
+## 08.01.2025:
+>Blackjack
 
-    creata la variabile dei soldi del giocatore
-    creato l'input per scegliere la puntata
+>importate le immagini delle carte
+
+>creata la variabile dei soldi del giocatore
+>creato l'input per scegliere la puntata
     
-    stampate le carte pescare per giocatore e bot
+>stampate le carte pescare per giocatore e bot
+
+## 09.01.2025:
+### aggiunte:
+>Puntata:<br>
+un bottone chiamato "place a bet" apre un @st.dialog con una st.form che richiede una puntata usando st.numer_input() e st.form_submit_button. La puntata viene salvata in st.session_state sotto la chiave "bet", che viene utilizzata per stampare la puntata, processo per il quale viene usato un blocco try perché all'inizio la chiave "bet" non è ancora stata creata e cercandola in st.session_state si crea un errore.
+
+### debug:
+>playerCards è una lista quindi si usa append<br>
+`playerCards.append(deck.draw())`
+
+>tolta la variabile dei soldi del giocatore perché inutile per ora
