2b2d9f13e95727ef347cb15c49716395c1982185
Emanuele Biddau <emabiddau@gmail.com>
2025-01-10 20:19:36 +0100

    Inizio partita con puntata e assegnazione carte mischiate

--- app.py
+++ app.py
@@ -8,47 +8,47 @@ st.set_page_config(
 card_width=105
 
 
-# number_of_decks = st.number_input("Number of decks", min_value=1, max_value=1, value=1)
+# st.session_state["iniziato"] = False
+# st.session_state["deck"] = Deck(1)
 
-deck = Deck(1)
+if "iniziato" not in st.session_state:
+    st.session_state["iniziato"] = False
 
-
-# st.markdown(f"## Deck created with {number_of_decks} deck/s")
-
-# st.image([card.image for card in deck.cards], width=card_width)
-
-if st.button("Shuffle"):
-    deck.shuffle()
-# st.image([card.image for card in deck.cards], width=card_width)
+if "deck" not in st.session_state:
+    st.session_state["deck"] = Deck(1)
 
 @st.dialog("Place a bet")
 def bet():
     with st.form("Bet", clear_on_submit=True, enter_to_submit=False, border=False):
 
-        bet = st.number_input("", min_value=1, max_value=500, value=1, key="bet")
+        bet = st.number_input("", min_value=1, max_value=500, value=1, key='bet')
 
         if st.form_submit_button("Invia"):
+            st.session_state["iniziato"] = True
+            st.session_state["deck"].shuffle()
             st.rerun()
 
-
-if st.button("Place a bet"):
-    bet()
-
-
-try:
-    st.text(f"The player has bet: {st.session_state['bet']}")
-except:
-    pass
+if st.session_state["iniziato"] == False:
+    if st.button("Place a bet"):
+        bet()
 
 
-playerCards: list[Card] = []
-botCards: list[Card] = []
+if st.session_state["iniziato"] == True:
+    
+    try:
+        st.text(f"The player has bet: {st.session_state['bet']}")
+    except:
+        pass
 
-for i in range(2):
-    playerCards.append(deck.draw())
-    botCards.append(deck.draw())
 
-st.image([card.image for card in playerCards], width=50)
-st.image([card.image for card in botCards], width=50)
+    playerCards: list[Card] = []
+    botCards: list[Card] = []
+
+    for i in range(2):
+        playerCards.append(st.session_state["deck"].draw())
+        botCards.append(st.session_state["deck"].draw())
+
+    st.image([card.image for card in playerCards], width=card_width)
+    st.image([card.image for card in botCards], width=card_width)
 
 st.write(st.session_state)
--- rapporto.md
+++ rapporto.md
@@ -9,6 +9,7 @@
 >creato l'input per scegliere la puntata
     
 >stampate le carte pescare per giocatore e bot
+<br>
 
 ## 09.01.2025:
 ### aggiunte:
@@ -20,3 +21,17 @@ un bottone chiamato "place a bet" apre un @st.dialog con una st.form che richied
 `playerCards.append(deck.draw())`
 
 >tolta la variabile dei soldi del giocatore perché inutile per ora
+<br>
+
+## 10.01.2025:
+### aggiunte:
+> usato st.session_state per memorizzare se la partita è iniziata o no con la chiave 'iniziato' e il mazzo con la chiave 'deck'. Il motivo è quello di poter controllare cosa viene riaggiornato e cosa no ogni volta che avviene un'interazione e quindi una rerun. In questo modo il mazzo non viene rimescolato ogni volta. Per accedere al mazzo corrente conviene quindi fare: <br> `st.session_state["deck"]`<br>e continuare da lì.
+
+> Adesso è possibile iniziare la partita subito piazzando una puntata. Questa, una volta fatta, viene memorizzata (lo scopo e quello di aggiungerla o di toglierla ai soldi del giocatore più avanti) e vengono pescate due carte.
+
+>Per ora vengono mostrate anche le carte del bot, più avanti questo non dovrà essere il caso. Le carte assegnate ai giocatori vengono salvate sotto `playerCards`, rispettivamente, `botCards`.
+
+### note:
+> i commenti<br>`# st.session_state["iniziato"] = False`<br>`# st.session_state["deck"] = Deck(1)`<br>
+servono per rapidamente resettare il session_state. Più avanti bisognerà automatizzare questo processo.
+<br>
